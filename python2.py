bf27461a8cbf2d7a754dcafebecb9e5bce931a71
VamshiBandi <vamshibandi520@gmail.com>
2026-07-28 15:38:57 +0530

    Feature branch second commit

--- python2.py
+++ python2.py
@@ -1,5 +1,5 @@
 # this is another python file to work with feature branch and merging
-
+# python program to check prime number or not
 def even(n):
     c=0
     for i in range(1,n+1):
@@ -12,4 +12,24 @@ def even(n):
         return f'{n} is a prime number'
     
 print(even(9))
+
+
+#Python program for two sum
+
+def finding(nums,target):
+    
+    l=0
+    diff=0
+    seen={}
+    for i,value in enumerate(nums):
+        diff=target-value
+        if diff in seen:
+            return [seen[diff],i]
+        
+        seen[value]=i
+    return []
+        
+target=10
+nums=[2,4,6,8,12,25]
+print(finding(nums,target))
             
