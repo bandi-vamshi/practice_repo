8f987b03b8ed041d95807ad04058bc8a5053affc
VamshiBandi <vamshibandi520@gmail.com>
2026-07-28 14:44:49 +0530

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # practice_repo
+
+# this file is used to track all the changes done in the files
--- python1.py
+++ python1.py
@@ -1,1 +1,6 @@
 print('vamshi')
+
+def vamshi(n1,n2):
+    return n1+n2
+
+print(f'the solution of te sum is {vamshi(10,20)}')
